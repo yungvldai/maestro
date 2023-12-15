655e44060b8223695d7ef9221c77b920b73ce209
Vladislav A. Ivanov <33333080+yungvldai@users.noreply.github.com>
2023-12-15 21:51:39 +0100

    fix: sequential start (#2)
    * fix: sequential start
    
    * chore: add compiler check to PR workflow
    
    * chore: run fmt
    
    * chore: add fmt hook

--- src/app/app_status.rs
+++ src/app/app_status.rs
@@ -2,9 +2,17 @@ use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum AppStatus {
+    // The app has been created, but no work has been done on it
     Init,
+
+    // The app has running process attached to it
     Running,
+
+    // Successfully invoked kill with the pid of the running process
     Stopping,
+
+    // The process has stopped. Having a process to stop is mandatory.
+    // There should be no transition Init -> Stopped
     Stopped,
 }
 
